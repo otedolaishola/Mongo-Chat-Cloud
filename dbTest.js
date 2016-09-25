09308c383629da2a1d1bac30b48d37209475578a
TimmyReilly <timreilly@live.com>
2016-09-24 22:49:42 -0700

    working locally

---
+++ dbTest.js
@@ -0,0 +1,20 @@
+var MongoClient = require('mongodb').MongoClient
+    , format = require('util').format;
+
+MongoClient.connect('mongodb://40.83.182.130:27017/test', function(err, db) {
+    if(err) throw err;
+
+    var collection = db.collection('test_insert');
+    collection.insert({a:2}, function(err, docs) {
+        collection.count(function(err, count) {
+            console.log(format("count = %s", count));
+        });
+    });
+
+    // Locate all the entries using find
+    collection.find().toArray(function(err, results) {
+        console.dir(results);
+        // Let's close the db
+        db.close();
+    });
+});
